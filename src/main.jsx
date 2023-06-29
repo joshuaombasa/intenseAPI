4cb3f85a8ecb7e54880f22b6471210a5af951470
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-29 23:19:23 +0300

    updated jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import BoxesContextProvider from './ BoxesContext.jsx'
+import BoxesContextProvider from './BoxesContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BoxesContextProvider>
